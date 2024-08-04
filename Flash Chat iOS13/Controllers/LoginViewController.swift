58efe7ca0b24331a4479afc5235704c73b93cbcd
Zak Ashour <ashourz@gmail.com>
2024-08-04 10:18:28 -0500

    first commit

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
+    override func viewDidLoad() {
+        title = K.appName
+    }
+    
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                guard let strongSelf = self else { return }
+                if let e = error{
+                    print(e.localizedDescription)
+                } else {
+                    strongSelf.performSegue(withIdentifier: K.loginSegue, sender: self)
+                }
+            }
+        }
     }
     
 }
--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
+    override func viewDidLoad() {
+        title = K.appName
+    }
+    
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text{
+                Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                    
+                    if let e = error{
+                        print(e.localizedDescription)
+                    } else {
+                        //Naivgate to chat view controller
+                        self.performSegue(withIdentifier: K.registerSegue, sender: self)
+                    }
+            }
+        }
     }
     
 }
